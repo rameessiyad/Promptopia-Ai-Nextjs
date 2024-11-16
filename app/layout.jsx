3b4dc707d326595c5cdb0e3f6c90cc7da47e18b4
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-16 08:29:58 +0530

    fixed error in Rootlayot

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ export const metaData = {
   description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = () => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="eng">
       <body>

32a77c07eed14bf1e211974317ca1830f5782dba
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-16 08:24:24 +0530

    layout file added

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+import "@styles/globals.css";
+
+export const metaData = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const layout = () => {
+  return <div>layout</div>;
+};
+
+export default layout;

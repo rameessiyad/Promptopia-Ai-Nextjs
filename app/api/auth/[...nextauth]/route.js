e11d41e4605288960b0e9a3dbd4503af70607569
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-16 12:13:24 +0530

    serverless route setup done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 console.log({
   GOOGLE_ID: process.env.GOOGLE_ID,
@@ -20,13 +21,21 @@ const handler = NextAuth({
       await connectToDB();
 
       // check if user already exists
-      
+      const userExists = await User.findOne({ email: profile.email });
+
       // if not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.image,
+        });
+      }
 
       return true;
     } catch (error) {
-        console.log(error);
-        return false;
+      console.log(error);
+      return false;
     }
   },
 });

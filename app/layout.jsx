d198ed2c230fcddc6a8d386afbb83fc9f0c1141f
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-16 08:59:23 +0530

    RootLayout updaed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@styles/globals.css";
+import Nav from "@components/Nav";
 
 export const metaData = {
   title: "Promptopia",
@@ -13,7 +14,10 @@ const RootLayout = ({ children }) => {
           <div className="gradient" />
         </div>
 
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
   );

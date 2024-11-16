a0c9e50550f4c8c9f536ee54a8f6f866e2af1c0c
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-16 12:06:09 +0530

    user schema created

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
 
 console.log({
   GOOGLE_ID: process.env.GOOGLE_ID,
@@ -14,7 +15,20 @@ const handler = NextAuth({
     }),
   ],
 
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      // check if user already exists
+      
+      // if not, create a new user
+
+      return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };

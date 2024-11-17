bce68754d32767d6272a75f9e964a828db3a1621
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-17 08:36:45 +0530

    prompt schema created

---
+++ models/prompt.js
@@ -0,0 +1,20 @@
+import { model, models, Schema } from "mongoose";
+
+const promptSchema = new Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is required"],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is required"],
+  },
+});
+
+const Prompt = models.Prompt || model("Prompt", promptSchema);
+
+export default Prompt;

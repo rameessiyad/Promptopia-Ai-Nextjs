07716bf419f2b54186e9f287c144087ac08882fa
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-17 21:59:43 +0530

    delete prompt api route created

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -33,3 +33,15 @@ export const PATCH = async (request, { params }) => {
     return new Response("Failed to update prompt", { status: 500 });
   }
 };
+
+//DELETE
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectToDB();
+
+    await Prompt.findByIdAndDelete(params.id);
+    return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+    return new Response("Failed to delete the prompt", { status: 500 });
+  }
+};

8c2faf86edccb27a3c1078114f0592b29fb0b5d8
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-17 15:52:38 +0530

    fetch posts added on myprofile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,13 +5,26 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState("");
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPost();
+  }, []);
+
   const handleEdit = () => {};
   const handleDelete = () => {};
   return (
     <Profile
       name="My"
       desc="Welcome to your personalized profile page"
-      data={[]}
+      data={posts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />

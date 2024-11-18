eb072554e50ac15dbd3b1dae9f15fd1cf323a432
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-18 15:55:40 +0530

    feat: prompt edit functionality added

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -24,37 +24,38 @@ const EditPrompt = () => {
     if (promptId) getPromptDetails();
   }, [promptId]);
 
-  //   const createPrompt = async (e) => {
-  //     e.preventDefault();
-  //     setSubmitting(true);
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
-  //     try {
-  //       const response = await fetch("/api/prompt/new", {
-  //         method: "POST",
-  //         body: JSON.stringify({
-  //           prompt: post.prompt,
-  //           userId: session?.user.id,
-  //           tag: post.tag,
-  //         }),
-  //       });
+    if (!promptId) return alert("Prompt not found");
 
-  //       if (response.ok) {
-  //         router.push("/");
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     } finally {
-  //       setSubmitting(false);
-  //     }
-  //   };
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
-      type="Update"
+      type="Edit"
       post={post}
       setPost={setPost}
       submitting={submitting}
-      //   handleSubmit={createPrompt}
+      handleSubmit={updatePrompt}
     />
   );
 };
